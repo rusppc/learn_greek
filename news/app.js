7fc5ffd16a3bcbe9b964bab6ad7a0a32dc09cd3f
rusppc <56300433+rusppc@users.noreply.github.com>
2024-05-15 16:05:27 +0300

    Update app.js

--- news/app.js
+++ news/app.js
@@ -127,7 +127,7 @@ function generateWords() {
   // Генерируем новые слова
   english_words = [];
   russian_words = [];
-  word_count = 4
+  word_count = 6
 
   for (var i = 0; i < word_count; i++) {
     var index = random(0, keys.length - 1);
